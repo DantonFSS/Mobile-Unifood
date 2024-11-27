54835289fec886093e6f72ed3905e94efdafb303
danton <dantonfilipe.s@gmail.com>
2024-11-27 00:51:18 -0300

    raw: add item announcment page files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
-  { path: 'announce', loadChildren: () => import('./pages/annouce/announce.module').then(m => m.AnnouncePageModule) },
+  { path: 'announce', loadChildren: () => import('./pages/announce/announce.module').then(m => m.AnnouncePageModule) },
   { path: 'map', loadChildren: () => import('./pages/map/map.module').then(m => m.MapPageModule) },
   { path: 'cart', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartPageModule) },
 ];
---
+++ src/app/pages/announce/announce.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announce',
+  templateUrl: './announce.page.html',
+  styleUrls: ['./announce.page.scss']
+})
+export class AnnouncePage {
+  constructor() {}
+
+  categories = [
+    { name: 'Doces', image: 'assets/images/doces.png' },
+    { name: 'Sanduíches', image: 'assets/images/sanduiches.png' },
+    { name: 'Pizzas', image: 'assets/images/pizzas.png' },
+    { name: 'Sorvete', image: 'assets/images/sorvetes.png'  },
+  ];
+
+}
+
